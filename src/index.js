e2b408b1f0719b8e46b478824edf84c19e2baff8
unknown <houzhihuil@gmail.com>
2023-06-06 20:40:57 -0400

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client'; 
+ 
+function MyComponent(){
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsloaded] = useState(false);
+    const [items, setItems] = useState([]);
 
+    // let url = "https://jsonplaceholder.typicode.com/photos"; 
+    
+    let url = "/produits.json";
+    useEffect(() => {
+        fetch(url)
+        .then(res => res.json())
+        .then(
+            (result) =>{
+                setIsloaded(true);
+                setItems(result.produits);
+            },
+            (error) =>{
+                setIsloaded(true);
+                setError(error);
+            }
+        )
+    }, [url] )
+    if(error){
+        return <div> Error: {error.message}</div>;
+    }else if (!isLoaded){
+        return <div> Chargement...</div>;
+    }else {
+        return ( 
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+             
+            <table border="1" width="60%">
+                <thead> 
+                    <tr>
+                        <th>Id</th>
+                        <th>Description</th>
+                        <th>Image</th>
+                        <th>Prix</th> 
+                        <th>Details</th> 
+                    </tr>
+                </thead>
+                <tbody>
+                {items.map(item =>(
+                     <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.description}</td>
+                        <td>
+                        <img src={item.image} alt="laptop.img" width="200"/>
+                        </td>
+                        <td>{item.prix}</td> 
+                        <td>{item.details}</td> 
+                     </tr>  
+                ))} 
+                </tbody>
+            </table> 
+            </div>
+        );
+    } 
+}
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<MyComponent />);  
+ 
